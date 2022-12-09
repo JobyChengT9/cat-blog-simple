27c1d9e9dee7e153d31629798cd6920c278b630e
JobyCheng <45289024+JobyCheng@users.noreply.github.com>
2022-12-10 02:08:23 +0800

    Website update

--- client/src/db.js
+++ client/src/db.js
@@ -1,9 +1,17 @@
+import cat1 from "./img/cat1.jpg"
+import cat2 from "./img/cat2.jpeg"
+import cat3 from "./img/cat3.jpg"
+import cat4 from "./img/cat4.jpg"
+import cat5 from "./img/cat5.jpg"
+import cat6 from "./img/cat6.jpg"
+
+
 export const db = [
       {
         id: 1,
         title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-        img: "cat1.jpg",
+        img: cat1,
         cat:"SOCIAL",
         username:"Tabby",
         userImg:"tabby.jpeg",
