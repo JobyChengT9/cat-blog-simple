7ebf7e06ef644487319b8bf251ac714357c05ca2
JobyCheng <45289024+JobyCheng@users.noreply.github.com>
2022-12-11 01:11:39 +0800

    Website update

--- client/src/db.js
+++ client/src/db.js
@@ -1,70 +1,71 @@
-import cat1 from "./img/cat1.jpg"
-import cat2 from "./img/cat2.jpeg"
-import cat3 from "./img/cat3.jpg"
-import cat4 from "./img/cat4.jpg"
-import cat5 from "./img/cat5.jpg"
-import cat6 from "./img/cat6.jpg"
-
+import cat1 from "./img/cat1.png";
+import cat2 from "./img/cat2.png";
+import cat3 from "./img/cat3.png";
+import cat4 from "./img/cat4.png";
+import cat5 from "./img/cat5.png";
+import cat6 from "./img/cat6.png";
+import tabby from "./img/tabby.png";
+import munchkin from "./img/munchkin.png";
 
 export const db = [
-      {
-        id: 1,
-        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-        img: cat1,
-        cat:"SOCIAL",
-        username:"Tabby",
-        userImg:"tabby.jpeg",
-        date:"2022-11-09 18:14:43",
-      },
-      {
-        id: 2,
-        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-        img: "cat2.jpeg",
-        cat:"HEALTH",
-        username:"Tabby",
-        userImg:"tabby.jpeg",
-        date:"2022-11-10 01:02:59",
-      },
-      {
-        id: 3,
-        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-        img: "cat3.jpg",
-        cat:"BEHAVIOUR",
-        username:"Munchkin",
-        userImg:"munchkin.jpeg",
-        date:"2022-11-15 19:56:43",
-      },
-      {
-        id: 4,
-        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-        img: "cat4.jpg",
-        cat:"ADOPTION",
-        username:"Munchkin",
-        userImg:"munchkin.jpeg",
-        date:"2022-11-16 19:59:43",
-      },
-      {
-        id: 5,
-        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-        img: "cat5.jpg",
-        cat:"SOCIAL",
-        username:"Munchkin",
-        userImg:"munchkin.jpeg",
-        date:"2022-11-28 02:59:43",
-      },
-      {
-        id: 6,
-        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-        img: "cat6.jpg",
-        cat:"HEALTH",
-        username:"Tabby",
-        userImg:"tabby.jpeg",
-        date:"2022-12-01 15:59:43",
-      },
-]
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    img: cat1,
+    cat: "SOCIAL",
+    username: "Tabby",
+    userImg: tabby,
+    date: "2022-11-09 18:14:43",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    img: cat2,
+    cat: "HEALTH",
+    username: "Tabby",
+    userImg: tabby,
+    date: "2022-11-10 01:02:59",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    img: cat3,
+    cat: "BEHAVIOUR",
+    username: "Munchkin",
+    userImg: munchkin,
+    date: "2022-11-15 19:56:43",
+  },
+  {
+    id: 4,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    img: cat4,
+    cat: "ADOPTION",
+    username: "Munchkin",
+    userImg: munchkin,
+    date: "2022-11-16 19:59:43",
+  },
+  {
+    id: 5,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    img: cat5,
+    cat: "SOCIAL",
+    username: "Munchkin",
+    userImg: munchkin,
+    date: "2022-11-28 02:59:43",
+  },
+  {
+    id: 6,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    img: cat6,
+    cat: "HEALTH",
+    username: "Tabby",
+    userImg: tabby,
+    date: "2022-12-01 15:59:43",
+  },
+];
