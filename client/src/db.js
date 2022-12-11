6189b19d3cfc4e91b2c9adaefc10af3de2f160cd
JobyCheng <45289024+JobyCheng@users.noreply.github.com>
2022-12-11 16:55:00 +0800

    Update db.js

--- client/src/db.js
+++ client/src/db.js
@@ -10,8 +10,8 @@ import munchkin from "./img/munchkin.png";
 export const db = [
   {
     id: 1,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    title: "Ut enim ad minim veniam",
+    desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
     img: cat1,
     cat: "SOCIAL",
     username: "Tabby",
@@ -30,8 +30,8 @@ export const db = [
   },
   {
     id: 3,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    title: "1914 translation by H. Rackham",
+    desc: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.",
     img: cat3,
     cat: "BEHAVIOUR",
     username: "Munchkin",
@@ -40,8 +40,8 @@ export const db = [
   },
   {
     id: 4,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    title: "Itaque earum rerum hic tenetur a sapiente",
+    desc: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided.",
     img: cat4,
     cat: "ADOPTION",
     username: "Munchkin",
@@ -50,8 +50,8 @@ export const db = [
   },
   {
     id: 5,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    title: "Cursus eget nunc scelerisque viverra",
+    desc: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
     img: cat5,
     cat: "SOCIAL",
     username: "Munchkin",
@@ -60,8 +60,8 @@ export const db = [
   },
   {
     id: 6,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+    title: "Ac placerat vestibulum lectus mauris ultrices ",
+    desc: "At lectus urna duis convallis convallis tellus. Iaculis at erat pellentesque adipiscing. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Risus sed vulputate odio ut enim blandit volutpat. Sodales neque sodales ut etiam sit amet nisl purus in. Faucibus turpis in eu mi bibendum neque. Sed id semper risus in hendrerit gravida rutrum. Mattis rhoncus urna neque viverra justo nec ultrices. Nec feugiat nisl pretium fusce id velit ut. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Amet porttitor eget dolor morbi non arcu risus quis varius. Malesuada pellentesque elit eget gravida cum sociis natoque.",
     img: cat6,
     cat: "HEALTH",
     username: "Tabby",
