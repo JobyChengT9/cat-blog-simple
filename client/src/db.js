3c932bbd0952391ff47d0a0dabd568be1dc51062
JobyCheng <45289024+JobyCheng@users.noreply.github.com>
2022-12-11 22:01:27 +0800

    Website update

--- client/src/db.js
+++ client/src/db.js
@@ -51,7 +51,7 @@ export const db = [
   {
     id: 5,
     title: "Cursus eget nunc scelerisque viverra",
-    desc: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+    desc: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \" de Finibus Bonorum et Malorum \" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
     img: cat5,
     cat: "SOCIAL",
     username: "Munchkin",
